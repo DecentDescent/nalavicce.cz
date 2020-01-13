0cf11051cae1843b6fc94a7a3aa2d52c3aa2737c
Jan Rádl <janradl@Jan-MacBook-Pro.local>
2020-01-13 22:16:47 +0100

    Finalizing Team

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,15 @@ import HowItWorks from "../components/HowItWorks";
 import Contact from "../components/Contact";
 import Team from "../components/Team";
 import Footer from "../components/Footer";
+import styles from "../styles/general.scss";
 class IndexPage extends Component {
   render() {
     return (
-      <>
+      <div className={styles["body"]}>
         <Head />
         <Team />
         <Footer />
-      </>
+      </div>
     );
   }
 }
